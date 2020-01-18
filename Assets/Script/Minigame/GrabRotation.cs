df5a3c6814d199966c946e74409054d0ce6b604e
Maisang <sarusasiri@gmail.com>
2020-01-18 19:43:27 +0700

    Make floder minigame

---
+++ Assets/Script/Minigame/GrabRotation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabRotation : MonoBehaviour
+{
+   
+    public float speedRotation = 5f;
+    [SerializeField]
+    public Vector3 originalRotation;
+
+
+    private void Start()
+    {
+        originalRotation = gameObject.transform.localEulerAngles;
+    }
+    void OnMouseDrag()
+    {
+        float XaxisRotaion = Input.GetAxis("Mouse X") * speedRotation;
+        float YaxisRotaion = Input.GetAxis("Mouse Y") * speedRotation;
+
+
+        //transform.Rotate(Vector3.right, YaxisRotaion);
+        transform.Rotate(Vector3.down, XaxisRotaion);
+
+    }
+  
+
+    private void OnMouseUp()
+    {
+        transform.eulerAngles = new Vector3(originalRotation.x, originalRotation.y, 0);
+        Debug.Log("Original");
+    }
+
+
+}
---
+++ Assets/Script/Minigame/TestRotae.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRotae : MonoBehaviour
+{
+    public Transform[] map;
+    public Transform currentObject;
+    int count = 0;
+
+
+    private float traitionSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentObject = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Count =" + count);
+        RunNumber();
+
+        //if (Input.GetKeyDown(KeyCode.Mouse1))
+        //{
+        //    currentView = view[1];
+        //    switchInput = false;
+        //}
+        //if (Input.GetKeyDown(KeyCode.Mouse2))
+        //{
+        //    currentView = view[0];
+        //    switchInput = false;
+        //}
+        //if (Input.GetKeyDown(KeyCode.Mouse0))
+        //{
+        //    currentView = view[2];
+        //    switchInput = true;
+        //}
+
+
+
+        switch (count)
+        {
+            case -1:
+                currentObject = map[1];
+                map[1].gameObject.SetActive(true);
+                map[2].gameObject.SetActive(false);
+                map[0].gameObject.SetActive(false);
+
+                break;
+
+            case 0:
+                currentObject = map[0];
+                map[0].gameObject.SetActive(true);
+                map[1].gameObject.SetActive(false);
+                map[2].gameObject.SetActive(false);
+
+                break;
+
+            case 1:
+
+                currentObject = map[2];
+                map[2].gameObject.SetActive(true);
+                map[1].gameObject.SetActive(false);
+                map[0].gameObject.SetActive(false);
+
+                break;
+
+
+        }
+
+
+        //ControlCamera();
+    }
+
+    public void RunNumber()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && count == 0)
+        {
+            count = 1;
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1) && count == 0)
+        {
+            count = -1;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1) && count == 1)
+        {
+            count -= 1;
+            Debug.Log(count + "test2");
+
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0) && count == -1)
+        {
+            count += 1;
+            Debug.Log(count + "test3");
+
+        }
+
+
+
+
+
+
+
+    }
+
+    private void LateUpdate()
+    {
+        //        //transform.position = player.transform.position + offset;
+        //        transform.position = Vector3.Lerp(transform.position, currentObject.transform.position, Time.deltaTime * traitionSpeed);
+        //        Vector3 currentAngle = new Vector3(
+        //            Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentObject.transform.rotation.eulerAngles.x, Time.deltaTime * traitionSpeed),
+        //            Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentObject.transform.rotation.eulerAngles.y, Time.deltaTime * traitionSpeed),
+        //            Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentObject.transform.rotation.eulerAngles.z, Time.deltaTime * traitionSpeed));
+
+        //        transform.eulerAngles = currentAngle;
+
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, currentObject.rotation, traitionSpeed);
+        transform.position = Vector3.Lerp(transform.position, currentObject.transform.position, Time.deltaTime * traitionSpeed);
+
+    }
+
+
+
+
+}
