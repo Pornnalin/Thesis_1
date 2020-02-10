cd288f940ab05b75c78e4872efce4a00958b6586
Maisang <sarusasiri@gmail.com>
2020-02-10 19:37:01 +0700

    save before chagne ver

---
+++ Assets/Script/MainGame/Laser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public LineRenderer line;
+    public Transform s;
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        //line.transform.position = s.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,transform.forward,out hit))
+        {
+            if (hit.collider)
+            {
+                line.SetPosition(1, new Vector3(0, 0, hit.distance));
+            }
+            else
+            {
+                line.SetPosition(1, new Vector3(0, 0, 5000));
+            }
+        }
+    }
+}
